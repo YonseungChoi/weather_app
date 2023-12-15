52ea216bdfea02ec38edacf2d07e7626d71f1ec9
YonseungChoi <sachoius@gmail.com>
2023-12-15 15:42:03 -0500

    first commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "61fc3c7278msh3e6adcc50164a44p166d20jsnbb83dde5f687",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "b346faf298019a7d194f7666d5ee5578"; 
